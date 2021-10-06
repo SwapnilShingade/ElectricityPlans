965d7928fdb422b1661b861eea4540441baa8853
SwapnilShingade <shingadeswsapnil23@gmail.com>
2021-10-06 19:38:03 +0530

    added auto mapper from mapping

---
+++ ElectricPlansApi/BusinessLayer/AutoMapper/Mappings.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BusinessLayer.Models;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.AutoMapper
+{
+    public class Mappings : Profile
+    {
+
+        public Mappings()
+        {
+            CreateMap<ProductsDTO, Product>();
+        }
+
+    }
+
+}
